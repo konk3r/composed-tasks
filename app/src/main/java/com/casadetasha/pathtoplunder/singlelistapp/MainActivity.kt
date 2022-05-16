7a06729f2cebc70eb9cae5d2a84fcc02bb0925de
Gabriel Spencer <gabrielspencer@gmail.com>
2022-05-15 21:32:26 -0700

    Separate viewModel from TaskList

--- app/src/main/java/com/casadetasha/pathtoplunder/singlelistapp/MainActivity.kt
+++ app/src/main/java/com/casadetasha/pathtoplunder/singlelistapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.casadetasha.pathtoplunder.singlelistapp
 
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,14 +8,12 @@ import androidx.activity.viewModels
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,79 +33,81 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TaskList()
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun TaskList() {
-        var isOpen by remember { mutableStateOf(false) }
-
-        LazyColumn {
-            items(viewModel.getTasks()) { task ->
-                TaskRow(AddTaskState(task)) { }
-            }
-
-            item {
-                Crossfade(
-                    targetState = isOpen,
-                    modifier = Modifier.animateContentSize()
-                ) { wasOpened ->
-                    when (wasOpened) {
-                        false -> TextButton(
-                                onClick = { isOpen = true },
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .fillMaxWidth(80f)
-                                    .animateContentSize()
-                            ) {
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    Text("Add task", fontWeight = Bold)
-                                    Text("+", fontSize = 18.sp, fontWeight = Bold)
-                                }
-                            }
-
-                        true -> TaskRow(AddTaskState(BlankTask(), isOpen = true)) { task ->
-                            if (task !is BlankTask) viewModel.addTask(task)
-                            isOpen = false
-                        }
+                    TaskList(viewModel.getTasks()) { newTask ->
+                        viewModel.addTask(newTask)
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun TaskList(tasks: List<Task>, addTask: (Task) -> Unit) {
+    var isOpen by remember { mutableStateOf(false) }
+
+    LazyColumn {
+        items(tasks) { task ->
+            TaskRow(AddTaskState(task)) { }
+        }
+
+        item {
+            Crossfade(
+                targetState = isOpen,
+                modifier = Modifier.animateContentSize()
+            ) { wasOpened ->
+                when (wasOpened) {
+                    false -> TextButton(
+                        onClick = { isOpen = true },
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(80f)
+                            .animateContentSize()
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text("Add task", fontWeight = Bold)
+                            Text("+", fontSize = 18.sp, fontWeight = Bold)
+                        }
+                    }
+
+                    true -> TaskRow(AddTaskState(BlankTask(), isOpen = true)) { task ->
+                        if (task !is BlankTask) addTask(task)
+                        isOpen = false
+                    }
+                }
+            }
+        }
+    }
 //        DragAndDropList(
 //            items = tasks,
 //            { from, to -> }
 //        )
-    }
+}
 
-    @Composable
-    private fun TaskRow(taskState: AddTaskState, onClose: (Task) -> Unit) {
-        val task = taskState.task
-        var isOpen by remember { mutableStateOf(taskState.isOpen) }
-        Button(
-            onClick = {
-                isOpen = !isOpen
-                if (!isOpen) onClose(task)
-                Log.d("Task", "Toggling open")
-            },
-            elevation = ButtonDefaults.elevation(
-                defaultElevation = 6.dp,
-                pressedElevation = 8.dp,
-                disabledElevation = 0.dp
-            ),
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(90f)
-                .animateContentSize()
-        ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = task.name)
-                if (isOpen) {
-                    TaskInput(task)
-                }
+@Composable
+private fun TaskRow(taskState: AddTaskState, onClose: (Task) -> Unit) {
+    val task = taskState.task
+    var isOpen by remember { mutableStateOf(taskState.isOpen) }
+    Button(
+        onClick = {
+            isOpen = !isOpen
+            if (!isOpen) onClose(task)
+            Log.d("Task", "Toggling open")
+        },
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 6.dp,
+            pressedElevation = 8.dp,
+            disabledElevation = 0.dp
+        ),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(90f)
+            .animateContentSize()
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = task.name)
+            if (isOpen) {
+                TaskInput(task)
             }
         }
     }
@@ -130,15 +129,16 @@ private fun TaskInput(task: Task) {
     Text(text = "Have cookie!")
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SingleListAppTheme {
-        Greeting("Android")
+        TaskList(createDummyTasks()) { }
+    }
+}
+
+private fun createDummyTasks(): List<Task> {
+    return (1..5).map {
+        CreatedTask("Task $it")
     }
 }
